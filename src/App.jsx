d50a8cd8258ca0390e8bd5fa3f69f00d7032ced1
saisashreek <saisashreekd@gmail.com>
2023-07-19 09:55:38 +0530

    Added Custom Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Register, Home } from "./Pages/index";
+import { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/register/" element={<Register />} exact />
+        <Route path="/login/" element={<Login />} exact />
+      </Routes>
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
       {/* <Home /> */}
-    </>
+    </BrowserRouter>
   );
 }
 
